d80aa89da84f2a211144ceb660cbcfa7eefc8ea5
niltonjrdev <dev40.nilton@gmail.com>
2025-12-09 23:11:45 -0300

    feat: ticket object created on create.js

--- src/controllers/tickets/create.js
+++ src/controllers/tickets/create.js
@@ -1,3 +1,16 @@
+import { randomUUID } from "node:crypto"
 export function create({ request, response }) {
-    response.end("Teste OK!")
+    const { equipment, description, user_name} = request.body
+
+    const ticket = {
+        id: randomUUID(),
+        equipment,
+        description,
+        user_name,
+        status: "open",
+        created_at: new Date(),
+        updated_at: new Date(),
+    }
+
+    response.end(JSON.stringify(ticket))
 }
