1d395bc7d207785892436ceb2e28f1df0669ca93
niltonjrdev <dev40.nilton@gmail.com>
2025-12-12 09:56:39 -0300

    feat: add function to get params route

--- src/controllers/tickets/index.js
+++ src/controllers/tickets/index.js
@@ -1,4 +1,8 @@
 export function index({ request, response, database }) {
+
+    const { status } = request.query
+
     const tickets = database.select("tickets")
+    
     return response.end(JSON.stringify(tickets))
 }
